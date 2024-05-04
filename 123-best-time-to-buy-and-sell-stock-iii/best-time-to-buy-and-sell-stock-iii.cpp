3b3b14980f436b2bb9a193be4eed2c045e07c138
Ritwik Khurana <41431727+ritwikkhurana@users.noreply.github.com>
2024-05-05 01:17:19 +0530

    Time: 104 ms (73.12%) | Memory: 77.6 MB (96.42%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<pair<int,int>>> dp(n, vector<pair<int,int>>(3, {0,0}));
         vector<pair<int,int>> curr(3, {0,0});
-        
-        dp[n-1][1] = {0,prices[n-1]};
-        dp[n-1][2] = {0,prices[n-1]};
+        vector<pair<int,int>> next(3, {0,0});
+        curr[1] = {0,prices[n-1]};
+        curr[2] = {0,prices[n-1]};
         for (int i = n-2 ; i>= 0 ; i--) {
             for (int j = 0 ; j <= 2 ; j++) {
                 if (j == 0) {
                     continue;
                 }
-                int buy = max(dp[i+1][j].first, -prices[i] + dp[i+1][j].second);
-                int sell = max(dp[i+1][j].second, prices[i] + dp[i+1][j-1].first);
-                dp[i][j] = {buy, sell};
+                int buy = max(curr[j].first, -prices[i] + curr[j].second);
+                int sell = max(curr[j].second, prices[i] + curr[j-1].first);
+                next[j] = {buy, sell};
             }
+            curr = next;
         }
-        return dp[0][2].first;
+        return curr[2].first;
     }
 };
