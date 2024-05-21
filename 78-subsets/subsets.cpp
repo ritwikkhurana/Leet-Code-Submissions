bda14f25a25bc52ca93b8cd0c6a9ed0cbea295ee
Ritwik Khurana <41431727+ritwikkhurana@users.noreply.github.com>
2024-05-21 22:35:41 +0530

    Time: 5 ms (14.01%) | Memory: 8.2 MB (94.64%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> output;
         int n = nums.size();
         int subsets = 1 << n;
+        vector<vector<int>> output;
         for (int i = 0 ; i < subsets ; i++) {
             vector<int> subset;
             for (int j = 0 ; j < n ; j++) {
