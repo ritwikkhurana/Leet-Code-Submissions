9fad884725cc59015e29a78dc9de78b3d137e972
Ritwik Khurana <41431727+ritwikkhurana@users.noreply.github.com>
2024-09-08 14:47:02 +0530

    Time: 17 ms (45.38%) | Memory: 18.6 MB (19.19%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    bool dfs(unordered_map<int,vector<int>> &adjList, int node, vector<int> &vis, vector<int> &pres, stack<int> &s) {
-        vis[node] = 1;
-        pres[node] = 1;
-        for (auto i: adjList[node]) {
-            if (!vis[i]) {
-                bool output = dfs(adjList, i, vis, pres, s);
-                if (!output) return false;
-            } else if (pres[i]) return false;
-        }
-        pres[node] = 0;
-        s.push(node);
-        return true;
-    }
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>> adjList;
+        vector<int> order;
+        int n = numCourses;
+        vector<int> inWt(n, 0);
+        unordered_map<int, vector<int>> adjList;
         for (auto i: prerequisites) {
             adjList[i[1]].push_back(i[0]);
+            inWt[i[0]]++;
         }
-        int n = numCourses;
-        vector<int> vis(n,0);
-        vector<int> pres(n,0);
-        vector<int> output;
-        stack<int> s;
+        queue<int> q;
         for (int i = 0 ; i < n ; i++) {
-            if (!vis[i]) {
-                bool res = dfs(adjList, i, vis, pres, s);
-                if (!res) return {};
+            if (inWt[i] == 0) q.push(i);
+        }
+        while (!q.empty()) {
+            int node = q.front();q.pop();
+            order.push_back(node);
+            for (auto i: adjList[node]) {
+                inWt[i]--;
+                if (inWt[i] == 0) q.push(i);
             }
         }
-        while (!s.empty()) {
-            output.push_back(s.top());s.pop();
+        for (int i = 0 ; i < n ; i++) {
+            if (inWt[i] != 0) return {};
         }
-        return output;
+        return order;
     }
 };
