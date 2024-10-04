aa158125e8698625c412e5ebb3b1e0af93d8d586
Ritwik Khurana <41431727+ritwikkhurana@users.noreply.github.com>
2024-10-04 17:47:12 +0530

    Time: 3 ms (50.09%) | Memory: 7.6 MB (77.65%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -10,24 +10,11 @@ public:
         int totalComb = getFact(currSize-1);
         int used = rem/totalComb;
         rem = rem%totalComb;
-        cout << "Used: " << used <<  ", Rem: " << rem << endl;
         if (rem == 0 && used == n) {
             sort(num.begin(), num.end(),greater<int>());
         } else if (rem == 0) {
-            for (int i = 0 ; i < n ; i++) {
-            cout << num[i] << " ";
-        }
-        cout << endl;
             swap(num[start],num[start+used-1]);
-            for (int i = 0 ; i < n ; i++) {
-            cout << num[i] << " ";
-        }
-        cout << endl;
             sort(num.begin() + start + 1, num.end(),greater<int>());
-            for (int i = 0 ; i < n ; i++) {
-            cout << num[i] << " ";
-        }
-        cout << endl;
         } else {
             swap(num[start], num[start+used]);
             sort(num.begin() + start + 1, num.end());
