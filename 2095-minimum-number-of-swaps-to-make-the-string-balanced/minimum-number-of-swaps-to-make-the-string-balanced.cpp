e6d27bfa7698d9e329572c5279cde08d032bb40c
Ritwik Khurana <41431727+ritwikkhurana@users.noreply.github.com>
2024-10-08 21:05:33 +0530

    Time: 97 ms (82.85%) | Memory: 32.7 MB (54.72%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        stack<char> bal;
-        int balanced = 0;
-        for (char c: s) {
-            if (c == '[') {
-                bal.push('[');
+        int open = 0,close=0,start=0,end=s.size()-1;
+        int swaps = 0;
+        while (start <= end) {
+            if (s[start] == '[') {
+                open++;
             } else {
-                if (!bal.empty()) {
-                    balanced++;
-                    bal.pop();
-                }
+                close++;
             }
+            if (close > open) {
+                while (end >= start && s[end] != '[') end--;
+                swap(s[start],s[end]);
+                swaps++;
+                end--;
+                open++;
+                close--;
+            }
+            start++;
         }
-        int totalPairs = s.size()/2;
-        int unbal = totalPairs - balanced;
-        vector<int> dp(unbal+1, 0);
-        if (unbal <= 2) return unbal == 0 ? 0 : 1;
-        dp[1] = 1;
-        dp[2] = 1;
-        for (int i = 3 ; i <= unbal ; i++) {
-            dp[i] = 1 + dp[i-2];
-        }
-        return dp[unbal]; 
+        return swaps;
     }
 };
