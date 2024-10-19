7144285029c33887a3acd51ec4b7ab1d83ef1aac
Ritwik Khurana <41431727+ritwikkhurana@users.noreply.github.com>
2024-10-19 17:56:14 +0530

    Time: 20 ms (100.00%) | Memory: 83.5 MB (63.49%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.cpp
+++ 1814-jump-game-vi/jump-game-vi.cpp
@@ -3,14 +3,13 @@ public:
     int maxResult(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> dp(n, 0);
-        priority_queue<pair<int,int>> pq;
+        deque<int> q;
         for (int i = n-1 ; i>=0; i--) {
             dp[i] += nums[i];
-            while (!pq.empty() && pq.top().second > (i+k)) pq.pop();
-            if (!pq.empty()) {
-                dp[i] += dp[pq.top().second];
-            }
-            pq.push({dp[i], i});
+            while (!q.empty() && q.front() > i+k) q.pop_front();
+            dp[i] += (q.empty() ? 0 : dp[q.front()]);
+            while (!q.empty() && dp[i] >= dp[q.back()]) q.pop_back();
+            q.push_back(i);
         }
         return dp[0];
     }
