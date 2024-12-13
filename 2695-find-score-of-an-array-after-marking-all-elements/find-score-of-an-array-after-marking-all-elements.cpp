009a15688741bec242c8f0d8c10260a9161aa11a
Ritwik Khurana <41431727+ritwikkhurana@users.noreply.github.com>
2024-12-13 18:19:43 +0530

    Time: 3 ms (99.64%) | Memory: 92.3 MB (99.67%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     long long findScore(vector<int>& nums) {
         long long finalScore = 0;
-        map<int,vector<int>> ourmap;
-        unordered_set<int> marked;
-        for (int i = 0 ; i < nums.size(); i++) {
-            ourmap[nums[i]].push_back(i);
-        }
-        for (auto i: ourmap) {
-            int val = i.first;
-            for (auto index : i.second) {
-                if (marked.find(index) == marked.end()) {
-                    finalScore += (long long)(val);
-                    marked.insert(index);
-                    if (index-1 >= 0) marked.insert(index-1);
-                    if (index+1 < nums.size()) marked.insert(index+1);
+        int start = 0, index=-1;
+        while (start < nums.size()) {
+            if (start == nums.size()-1 || nums[start+1] >= nums[start]) {
+                finalScore += nums[start];
+                for (int i = start-2; i>=0 && i> index ; i=i-2) {
+                    finalScore += nums[i];
                 }
+                index = start+1;
+                start += 2;
+            } else {
+                start++;
             }
         }
         return finalScore;
