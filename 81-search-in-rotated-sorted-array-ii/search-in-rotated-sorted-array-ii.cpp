453fff90712ca8b90dd5448272753497d4a6bd01
Ritwik Khurana <41431727+ritwikkhurana@users.noreply.github.com>
2025-01-02 13:44:46 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (13.00%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int start=0,end=n-1;
+                int start=0,end=nums.size()-1;
         while (start <= end) {
             int mid = start + (end-start)/2;
             if (nums[mid] == target) return true;
             if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
                 start++;
                 end--;
-                continue;
-            }
-            if (nums[start] < nums[end]) {
-                if (target > nums[mid]) {
+            } else if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target <= nums[mid]) {
+                    end = mid-1;
+                } else {
+                    start = mid+1;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[end]) {
                     start = mid+1;
                 } else {
                     end = mid-1;
-                }
-            } else {
-                if (nums[mid] >= nums[start]) {
-                    if (target < nums[mid] && target >= nums[start]) {
-                        end = mid-1;
-                    } else {
-                        start = mid+1;
-                    }
-                } else {
-                    if (target > nums[mid] && target <= nums[end]) {
-                        start = mid+1;
-                    } else {
-                        end = mid-1;
-                    }
                 }
             }
         }
