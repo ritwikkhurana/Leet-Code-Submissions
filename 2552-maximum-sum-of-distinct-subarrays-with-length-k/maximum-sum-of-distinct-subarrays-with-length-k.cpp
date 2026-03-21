9c65825a8aaa245441076d167a048cd64d4a1745
Ritwik Khurana <41431727+ritwikkhurana@users.noreply.github.com>
2026-03-21 20:11:31 +0530

    Time: 107 ms (46.28%) | Memory: 95.3 MB (60.34%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> ourmap;
-        long long maxSum = 0;
-        long long currSum = 0;
+        unordered_map<int,int> dist;
         int start=0,end=0;
+        long long sum=0,maxSum=0;
         while (end < nums.size()) {
-            int currSize = end-start+1;
-            currSum += nums[end];
-            ourmap[nums[end]]++;
-            if (currSize > k) {
-                ourmap[nums[start]]--;
-                if (ourmap[nums[start]] == 0) ourmap.erase(nums[start]);
-                currSum -= nums[start];
+            dist[nums[end]]++;
+            sum += nums[end];
+            if (end-start+1 == k) {
+                if (dist.size() == k) {
+                    if (sum > maxSum) maxSum = sum;
+                }
+                dist[nums[start]]--;
+                if (dist[nums[start]] == 0) dist.erase(nums[start]);
+                sum -= nums[start];
                 start++;
-                currSize--;
-            }
-            if (currSize == k && ourmap.size() == k) {
-                maxSum = max(maxSum, currSum);
             }
             end++;
         }
